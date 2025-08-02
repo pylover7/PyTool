d5c634ab6c070b578e4a39f5b6ee9e807fa638b3
pylover <1372755472@qq.com>
2025-08-02 21:05:39 +0800

    🐎 ci: 新增环境变量

--- backend/app/settings/__init__.py
+++ backend/app/settings/__init__.py
@@ -27,7 +27,7 @@ def parse_cors(v: Any) -> list[str] | str:
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file_encoding="utf-8",
-        env_file=[".env.local", ".env"],
+        env_file=["../.env"],
         env_ignore_empty=True,
         extra="ignore"
     )
