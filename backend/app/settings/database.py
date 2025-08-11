cdb587caf3b5d0d1238995cc8af045a89772883f
pylover <1372755472@qq.com>
2025-08-11 17:32:01 +0800

    ✨ feat: 丰富数据库的设置

--- backend/app/settings/database.py
+++ backend/app/settings/database.py
@@ -1,2 +1,20 @@
-def db_engine():
-  pass
+from pydantic import (
+    AnyUrl,
+    BeforeValidator,
+    HttpUrl,
+    PostgresDsn,
+    computed_field,
+    model_validator,
+)
+from pydantic_core import MultiHostUrl
+
+
+def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
