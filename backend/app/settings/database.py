e5586377cbfd3f04e45bd4918c83ee2963e29845
pylover <1372755472@qq.com>
2025-08-11 17:37:31 +0800

    ✨ feat: 支持多种数据库连接配置

--- backend/app/settings/database.py
+++ backend/app/settings/database.py
@@ -3,18 +3,20 @@ from pydantic import (
     BeforeValidator,
     HttpUrl,
     PostgresDsn,
+    MySQLDsn,
     computed_field,
     model_validator,
 )
 from pydantic_core import MultiHostUrl
+from typing import Optional
 
 
-def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
-        )
+def db_engine(scheme: str, username: str = "", password: str = "", host: str = "", port: int = 5432, path: str = "") -> Optional[PostgresDsn, MySQLDsn]:
+      return MultiHostUrl.build(
+          scheme="postgresql+psycopg",
+          username=self.POSTGRES_USER,
+          password=self.POSTGRES_PASSWORD,
+          host=self.POSTGRES_SERVER,
+          port=self.POSTGRES_PORT,
+          path=self.POSTGRES_DB,
+      )
